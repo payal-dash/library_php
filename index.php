5f30094b9d28db18abf55da9b55eb8911714d5d6
Payal dash <payaldash406@gmail.com>
2023-04-06 00:30:50 +0530

    files added

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="4.5;url=index-2.php">
+    <title>Loader</title>
+</head>
+<style>
+    .idd{
+        padding:20%;
+    }
+</style>
+<body>
+    <center><img src="tom.gif" alt="" class='idd'></center>
+</body>
+</html>
+
